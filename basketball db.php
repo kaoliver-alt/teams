c1ac4141d5333fe0c91887b272fe73d46886453e
kaoliver-alt <kaoliver@uncg.edu>
2022-07-29 07:02:15 -0400

    Connect to db. Query names of teams

--- basketball db.php
+++ basketball db.php
@@ -1,12 +1,12 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
 $dbname = "basketball";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
@@ -14,16 +14,44 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully <br>";
 
-$sql = "SELECT name FROM teams WHERE city = 'Brooklyn'";
+// Establish query
+$sql = "SELECT,
+FROM
+ORDER BY ";
+
+USE teams;
+
+$sql = "SELECT *
+FROM teams
+WHERE name = 'Lakers'
+ORDER BY city";
+
+
+// Query: What's the name of the team in Brooklyn?
+/* $sql = "SELECT name FROM teams WHERE city = 'Brooklyn'";
     $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "name: " . $row["name"]. "<br>";
     }
 } else {
     echo "0 results";
+}*/
+
+
+// Query: What are the cities and states of teams in the NBA?
+/* $sql = "SELECT name, city, state FROM teams WHERE league='NBA'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "name: " . $row["name"]. " - teams: " . $row["city"]. " " . $row["state"]. "<br>";
+    }
+} else {
+    echo "0 results";
 }
-$conn->close();
+$conn->close();*/
+
 ?>
